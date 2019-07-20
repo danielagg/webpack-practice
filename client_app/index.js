d05afa79342e84ec90e6048d9b9b06ebb8ae5659
Daniel Agg <daniel.agg@outlook.com>
2019-07-20 13:58:03 +0200

    Skeletons, dependencies

---
+++ client_app/index.js
@@ -0,0 +1,10 @@
+var React = require("react");
+var ReactDOM = require("react-dom");
+
+require("index.css");
+
+const App = () => {
+  return <div>Hello.</div>;
+};
+
+ReactDOM.render(<App />, document.getElementById("app"));
